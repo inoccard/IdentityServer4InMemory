d97384a01e67b7e42b4e060f6cd55809776e4f70
inoccard <inocenciocardoso19@gmail.com>
2022-06-17 09:36:06 -0300

    added PostLogoutRedirectUri

--- Config.cs
+++ Config.cs
@@ -35,7 +35,7 @@ namespace IdSrvInMem
                     AllowedGrantTypes = GrantTypes.Implicit, // infica como a aplicação vai acessar este servidor de identidade, qual o fluxo
                     AllowAccessTokensViaBrowser = true,
                     RedirectUris = { "http://localhost:7220/signin-oidc" },
-                    PostLogoutRedirectUris = { "http://localhost:7220" },
+                    PostLogoutRedirectUris = { "http://localhost:7220/signout-callback-oidc" },
                     //ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
                     AllowedScopes = { 
                         "openid",
